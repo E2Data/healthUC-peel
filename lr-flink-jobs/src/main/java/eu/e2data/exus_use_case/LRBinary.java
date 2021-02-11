fe3110605af350e69290851835493d770efc70c0
Pantelis Linardatos <p.linardatos@exus.co.uk>
2021-02-11 16:46:52 +0200

    LRBinary update from flink-tornado-internal

--- lr-flink-jobs/src/main/java/eu/e2data/exus_use_case/LRBinary.java
+++ lr-flink-jobs/src/main/java/eu/e2data/exus_use_case/LRBinary.java
@@ -1,22 +1,4 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-package eu.e2data.exus_use_case;
+package org.apache.flink.examples.java.exus;
 
 import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.common.functions.MapFunction;
@@ -24,148 +6,100 @@ import org.apache.flink.api.common.functions.ReduceFunction;
 import org.apache.flink.api.common.functions.RichMapFunction;
 import org.apache.flink.api.common.io.FileInputFormat;
 import org.apache.flink.api.common.io.FileOutputFormat;
-import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.api.java.DataSet;
-import org.apache.flink.api.common.JobExecutionResult;
 import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.Utils;
 import org.apache.flink.api.java.io.TypeSerializerInputFormat;
 import org.apache.flink.api.java.io.TypeSerializerOutputFormat;
 import org.apache.flink.api.java.operators.IterativeDataSet;
 import org.apache.flink.api.java.tuple.Tuple1;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.tuple.Tuple4;
+import org.apache.flink.api.java.typeutils.TupleTypeInfo;
 import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.api.scala.typeutils.Types;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.core.fs.FileSystem;
 import org.apache.flink.util.Collector;
 
-import java.io.Serializable;
 import java.util.Collection;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-/**
- * TODO: add documentation
- */
 @SuppressWarnings("serial")
-public class LRBinary {
+public class ExusFlinkTornado {
 
 	public static void main(String[] args) throws Exception {
 
 		// Checking input parameters
 		final ParameterTool params = ParameterTool.fromArgs(args);
-
 		// set up execution environment
 		ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+
 		env.getConfig().setGlobalJobParameters(params); // make parameters available in the web interface
 
 		//default params
 		int numberOfFeatures = 82;
-
 		// convert to binary
-		FileOutputFormat<Data>
-				of = new TypeSerializerOutputFormat<>();
+
+		FileOutputFormat<Tuple2<double[], Double>>
+			of = new TypeSerializerOutputFormat<>();
 		env.readCsvFile(params.get("train")).fieldDelimiter("\t").ignoreFirstLine().types(String.class)
-				.flatMap(new ConvertToIndexMatrix())
-				.write(of, params.get("binary"), FileSystem.WriteMode.OVERWRITE);
+			.flatMap(new ConvertToIndexMatrix(numberOfFeatures))
+			.write(of, params.get("binary"), FileSystem.WriteMode.OVERWRITE);
 		env.execute();
 
-//		System.out.println(String.format("Conversion runtime: %d ms", env.getLastJobExecutionResult()
-//				.getNetRuntime(TimeUnit.MILLISECONDS)));
+		//System.out.println(String.format("Conversion runtime: %d ns", env.getLastJobExecutionResult()
+		//		.getNetRuntime(TimeUnit.NANOSECONDS)));
 
-		// input formats
-		FileInputFormat<Data> inputFormat = new TypeSerializerInputFormat<>(
-				TypeInformation.of(Data.class));
+		final TupleTypeInfo<Tuple2<double[], Double>> typeInfo = new TupleTypeInfo(Types.PRIMITIVE_ARRAY(Types.DOUBLE()), Types.DOUBLE());
+
+		FileInputFormat<Tuple2<double[], Double>> inputFormat = new TypeSerializerInputFormat<>(typeInfo);
 
 		//read binary training data
-		DataSet<Data> data = env.readFile(inputFormat, params.get("binary"));
+		DataSet<Tuple2<double[], Double>> data = env.readFile(inputFormat, params.get("binary"));
 		//Initialize W and b
-		DataSet<Params> parameters = env.fromElements(new Params(new double[numberOfFeatures + 1]));
+		DataSet<Tuple2<double[], Integer>> parameters = env.fromElements(new Tuple2(new double[numberOfFeatures + 1], numberOfFeatures));
 
-		// set number of bulk iterations for KMeans algorithm
-		IterativeDataSet<Params> loop = parameters.iterate(params.getInt("iterations", 10));
-
-		//train
-		DataSet<Params> newParameters = data
-				// compute a single step using every sample
-				.map(new SubUpdate()).withBroadcastSet(loop, "parameters")
-				// sum up all the steps
-				.reduce(new UpdateAccumulator(numberOfFeatures))
-				// average the steps and update all parameters
-				.map(new Update());
-
-		// feed new parameters back into next iteration
-		DataSet<Params> final_params = loop.closeWith(newParameters);
-
-		//read train data-set from csv
 		DataSet<Tuple1<String>> test_csv = env.readCsvFile(params.get("test")).fieldDelimiter("\t").ignoreFirstLine().types(String.class);
 		//convert to Data (X, y)
-		DataSet<Data> test_data = test_csv.flatMap(new ConvertToIndexMatrix());
-		//evaluate results
-		DataSet<String> results = test_data
-				.map(new Predict()).withBroadcastSet(final_params, "params")
-				.reduce(new Evaluate())
-				.map(new ComputeMetrics());
+		List<Tuple2<double[], Double>> list = test_csv.flatMap(new ConvertToIndexMatrix(numberOfFeatures)).collect();
+		DataSet<Tuple2<double[], Double>> test_data = env.fromCollection(list);
 
-		// emit result
-		JobExecutionResult perf_res = null;
-		if (params.has("output")) {
+		//System.setProperty("tornado", "true");
+		// Utils.tornadoVM = true;
+		IterativeDataSet<Tuple2<double[], Integer>> loop = parameters.iterate(params.getInt("iterations", 10));
+		DataSet<Tuple2<double[], Integer>> newParameters = data
+			// compute a single step using every sample
+			.map(new SubUpdate()).withBroadcastSet(loop, "parameters")
+			// sum up all the steps
+			.reduce(new UpdateAccumulator())
+			// average the steps and update all parameters
+			.map(new Update());
 
-			results
-					.writeAsText(params.get("output"), FileSystem.WriteMode.OVERWRITE);
+		//feed new parameters back into next iteration
+		DataSet<Tuple2<double[], Integer>> final_paramsT = loop.closeWith(newParameters);
 
-			// since file sinks are lazy, we trigger the execution explicitly
-			perf_res = env.execute("Exus Use Case");
-		} else {
-			System.out.println("Printing result to stdout. Use --output to specify output path.");
-			results.print();
-		}
+		DataSet<Tuple4<Double, Double, Double, Double>> results = test_data
+			.map(new Predict()).withBroadcastSet(final_paramsT, "params")
+			.reduce(new Evaluate())
+			.map(new ComputeMetrics());
 
-		System.out.println(String.format("LR Job runtime: %d ms", perf_res
-				.getNetRuntime(TimeUnit.MILLISECONDS)));
+		results.collect();
 
 	}
 
-	// *************************************************************************
-	//     DATA SOURCE READING (POINTS AND CENTROIDS)
-	// *************************************************************************
-	static class Data implements Serializable {
-		double[] X;
-		double y;
+	public static class ConvertToIndexMatrix implements FlatMapFunction<Tuple1<String>, Tuple2<double[], Double>> {
 
-		Data(double[] x, double y) {
-			X = x;
-			this.y = y;
+		private int n;
+
+		ConvertToIndexMatrix(int n) {
+			this.n = n;
 		}
-	}
-
-	/**
-	 * A set of parameters -- theta_0, theta_1, ... , theta_n
-	 */
-
-	static class Params {
-		double[] W;
-		final int n = 82;
-
-		Params(double[] w) {
-			W = w;
-		}
-
-		public Params div(Integer a) {
-			for (int j = 0; j < n + 1; j++) {
-				this.W[j] = W[j] / a;
-			}
-			return this;
-		}
-	}
-
-
-	public static class ConvertToIndexMatrix implements FlatMapFunction<Tuple1<String>, Data> {
-
-		final int n = 82;
 
 
 		@Override
-		public void flatMap(Tuple1<String> stringTuple1, Collector<Data> collector) {
+		public void flatMap(Tuple1<String> stringTuple1, Collector<Tuple2<double[], Double>> collector) {
 			String line = stringTuple1.f0;
 			int col = -1;
 			double[] X = new double[n + 1];
@@ -174,21 +108,17 @@ public class LRBinary {
 
 				//when col is -1 , then take the row number , else collect values
 				if (col > -1 && col < n) {
-					X[col] = cell != null ? Double.parseDouble(cell) : 0.0;
+					X[col] = cell != null ? Double.valueOf(cell) : 0.0;
 				} else if (col == n) {
 					X[col] = 1.0;
-					y = Double.parseDouble(cell);
+					y = Double.valueOf(cell);
 				}
 				col++;
 			}
 
-			collector.collect(new Data(X, y));
+			collector.collect(new Tuple2(X, y));
 		}
 	}
-
-	// *************************************************************************
-	//     DATA TYPES
-	// *************************************************************************
 
 	// *************************************************************************
 	//     USER FUNCTIONS
@@ -197,15 +127,11 @@ public class LRBinary {
 	/**
 	 * Compute a single BGD type update for every parameters.
 	 */
-	public static class SubUpdate extends RichMapFunction<Data, Tuple2<Params, Integer>> {
+	public static class SubUpdate extends RichMapFunction<Tuple2<double[], Double>, Tuple2<Tuple2<double[], Integer>, Integer>> {
 
-		Collection<Params> parameters;
+		private Collection<Tuple2<double[], Integer>> parameters;
 
-		int count = 1;
-
-		final int n = 82;
-		final double lr = 0.1;
-
+		private Tuple2<double[], Integer> parameter;
 
 		/**
 		 * Reads the parameters from a broadcast variable into a collection.
@@ -216,22 +142,26 @@ public class LRBinary {
 		}
 
 		@Override
-		public Tuple2<Params, Integer> map(Data in) {
+		public Tuple2<Tuple2<double[], Integer>, Integer> map(Tuple2<double[], Double> in) {
 
-			Params parameter = this.parameters.iterator().next();
+			int count = 1;
+			int n = 82;
+			double lr = 0.1;
+
+			parameter = (Tuple2<double[], Integer>) ((List) parameters).get(0);
 
 			double z = 0.0;
 			for (int j = 0; j < n + 1; j++) {
-				z += in.X[j] * parameter.W[j];
+				z += in.f0[j] * parameter.f0[j];
 			}
 
-			double error = (double) 1 / (1 + Math.exp(-z)) - in.y;
+			double error = (double) 1 / (1 + Math.exp(-z)) - in.f1;
 
 			for (int j = 0; j < n + 1; j++) {
-				in.X[j] = parameter.W[j] - lr * (error * in.X[j]);
+				in.f0[j] = parameter.f0[j] - lr * (error * in.f0[j]);
 			}
 
-			return new Tuple2<>(new Params(in.X), count);
+			return new Tuple2<>(new Tuple2(in.f0, n), count);
 
 		}
 	}
@@ -239,14 +169,11 @@ public class LRBinary {
 	/**
 	 * Compute TP,TN,FP,FN
 	 */
-	public static class Predict extends RichMapFunction<Data, Tuple4<Integer, Integer, Integer, Integer>> {
+	public static class Predict extends RichMapFunction<Tuple2<double[], Double>, Tuple4<Integer, Integer, Integer, Integer>> {
 
-		Collection<Params> parameters;
+		private Collection<Tuple2<double[], Integer>> parameters;
 
-		Params parameter;
-
-		final int n = 82;
-
+		private Tuple2<double[], Integer> parameter;
 
 		/**
 		 * Reads the parameters from a broadcast variable into a collection.
@@ -257,28 +184,43 @@ public class LRBinary {
 		}
 
 		@Override
-		public Tuple4<Integer, Integer, Integer, Integer> map(Data in) {
+		public Tuple4<Integer, Integer, Integer, Integer> map(Tuple2<double[], Double> in) {
 
-			this.parameter = this.parameters.iterator().next();
+			int n = 82;
+
+			parameter = (Tuple2<double[], Integer>) ((List) parameters).get(0);
 
 			double z = 0.0;
 			for (int j = 0; j < n + 1; j++) {
-				z += in.X[j] * parameter.W[j];
+				z += in.f0[j] * parameter.f0[j];
 			}
 
 			double predict = ((double) 1 / (1 + Math.exp(-z))) > 0.5 ? 1.0 : 0.0;
 
-			if (predict == 0.0 && in.y == 0.0) {
-				return new Tuple4<>(0, 1, 0, 0); // tn
-			} else if (predict == 0.0 && in.y == 1.0) {
-				return new Tuple4<>(0, 0, 0, 1); // fn
-			} else if (predict == 1.0 && in.y == 0.0) {
-				return new Tuple4<>(0, 0, 1, 0); // fp
-			} else if (predict == 1.0 && in.y == 1.0) {
-				return new Tuple4<>(1, 0, 0, 0); // tp
-			} else {
-				return new Tuple4<>(0, 0, 0, 0); // tp
+			int f0 = -1, f1 = -1, f2 = -1, f3 = -1;
+			if (predict == 0.0 && in.f1 == 0.0) {
+				f0 = 0; f1 = 1; f2 = 0; f3 = 0;
+				//return new Tuple4<>(0, 1, 0, 0); // tn
 			}
+			if (predict == 0.0 && in.f1 == 1.0) {
+				f0 = 0; f1 = 0; f2 = 0; f3 = 1;
+				///return new Tuple4<>(0, 0, 0, 1); // fn
+			}
+			if (predict == 1.0 && in.f1 == 0.0) {
+				f0 = 0; f1 = 0; f2 = 1; f3 = 0;
+				//return new Tuple4<>(0, 0, 1, 0); // fp
+			}
+			if (predict == 1.0 && in.f1 == 1.0) {
+				f0 = 1; f1 = 0; f2 = 0; f3 = 0;
+				//return new Tuple4<>(1, 0, 0, 0); // tp
+			}
+
+			if (in.f1 != 1.0 && in.f1 != 0.0) {
+				f0 = 0; f1 = 0; f2 = 0; f3 = 0;
+			} // tp
+
+
+			return new Tuple4<>(f0, f1, f2, f3);
 
 		}
 	}
@@ -286,19 +228,15 @@ public class LRBinary {
 	/**
 	 * Accumulator all the update.
 	 */
-	public static class UpdateAccumulator implements ReduceFunction<Tuple2<Params, Integer>> {
+	public static class UpdateAccumulator implements ReduceFunction<Tuple2<Tuple2<double[], Integer>, Integer>> {
 
-		private int n;
-
-		UpdateAccumulator(int n) {
-			this.n = n;
-		}
+		private int n = 82;
 
 		@Override
-		public Tuple2<Params, Integer> reduce(Tuple2<Params, Integer> val1, Tuple2<Params, Integer> val2) {
+		public Tuple2<Tuple2<double[], Integer>, Integer> reduce(Tuple2<Tuple2<double[], Integer>, Integer> val1, Tuple2<Tuple2<double[], Integer>, Integer> val2) {
 
 			for (int j = 0; j < n + 1; j++) {
-				val1.f0.W[j] = val1.f0.W[j] + val2.f0.W[j];
+				val1.f0.f0[j] = val1.f0.f0[j] + val2.f0.f0[j];
 			}
 			return new Tuple2<>(val1.f0, val1.f1 + val2.f1);
 
@@ -316,25 +254,31 @@ public class LRBinary {
 	/**
 	 * Compute the final update by average them.
 	 */
-	public static class Update implements MapFunction<Tuple2<Params, Integer>, Params> {
+	public static class Update implements MapFunction<Tuple2<Tuple2<double[], Integer>, Integer>, Tuple2<double[], Integer>> {
 
 		@Override
-		public Params map(Tuple2<Params, Integer> arg0) {
-			return arg0.f0.div(arg0.f1);
+		public Tuple2<double[], Integer> map(Tuple2<Tuple2<double[], Integer>, Integer> arg0) {
+			Tuple2<double[], Integer> arg1 = arg0.f0;
+			Integer arg2 = arg0.f1;
+			for (int i = 0; i < arg1.f0.length; i++) {
+				arg1.f0[i] = arg1.f0[i] / arg2;
+			}
+			return new Tuple2<>(arg1.f0, arg1.f1);
 		}
 
 	}
 
 
-	public static class ComputeMetrics implements MapFunction<Tuple4<Integer, Integer, Integer, Integer>, String> {
+	public static class ComputeMetrics implements MapFunction<Tuple4<Integer, Integer, Integer, Integer>, Tuple4<Double, Double, Double, Double>> {
 		@Override
-		public String map(Tuple4<Integer, Integer, Integer, Integer> v) {
+		public Tuple4<Double, Double, Double, Double> map(Tuple4<Integer, Integer, Integer, Integer> v) {
 			double acc = 1.0 - (double) (v.f2 + v.f3) / (v.f0 + v.f1 + v.f2 + v.f3);
 			double pr = (double) v.f0 / (v.f0 + v.f2);
 			double rec = (double) v.f0 / (v.f0 + v.f3);
 			double f1 = (2 * pr * rec) / (pr + rec);
-			return "ACCURACY: " + acc + "\n" + "PRECISION: " + pr + "\n" + "RECALL: " + rec + "\n" + "f1 MEASURE: " + f1 + "\n";
+			return new Tuple4(acc, pr, rec, f1);
 		}
 
 	}
+
 }
---
+++ lr-flink-jobs/src/main/java/eu/e2data/exus_use_case/LRBinaryBackup.java
@@ -0,0 +1,340 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package eu.e2data.exus_use_case;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.functions.ReduceFunction;
+import org.apache.flink.api.common.functions.RichMapFunction;
+import org.apache.flink.api.common.io.FileInputFormat;
+import org.apache.flink.api.common.io.FileOutputFormat;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.common.JobExecutionResult;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.io.TypeSerializerInputFormat;
+import org.apache.flink.api.java.io.TypeSerializerOutputFormat;
+import org.apache.flink.api.java.operators.IterativeDataSet;
+import org.apache.flink.api.java.tuple.Tuple1;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.tuple.Tuple4;
+import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.core.fs.FileSystem;
+import org.apache.flink.util.Collector;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * TODO: add documentation
+ */
+@SuppressWarnings("serial")
+public class LRBinary {
+
+	public static void main(String[] args) throws Exception {
+
+		// Checking input parameters
+		final ParameterTool params = ParameterTool.fromArgs(args);
+
+		// set up execution environment
+		ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+		env.getConfig().setGlobalJobParameters(params); // make parameters available in the web interface
+
+		//default params
+		int numberOfFeatures = 82;
+
+		// convert to binary
+		FileOutputFormat<Data>
+				of = new TypeSerializerOutputFormat<>();
+		env.readCsvFile(params.get("train")).fieldDelimiter("\t").ignoreFirstLine().types(String.class)
+				.flatMap(new ConvertToIndexMatrix())
+				.write(of, params.get("binary"), FileSystem.WriteMode.OVERWRITE);
+		env.execute();
+
+//		System.out.println(String.format("Conversion runtime: %d ms", env.getLastJobExecutionResult()
+//				.getNetRuntime(TimeUnit.MILLISECONDS)));
+
+		// input formats
+		FileInputFormat<Data> inputFormat = new TypeSerializerInputFormat<>(
+				TypeInformation.of(Data.class));
+
+		//read binary training data
+		DataSet<Data> data = env.readFile(inputFormat, params.get("binary"));
+		//Initialize W and b
+		DataSet<Params> parameters = env.fromElements(new Params(new double[numberOfFeatures + 1]));
+
+		// set number of bulk iterations for KMeans algorithm
+		IterativeDataSet<Params> loop = parameters.iterate(params.getInt("iterations", 10));
+
+		//train
+		DataSet<Params> newParameters = data
+				// compute a single step using every sample
+				.map(new SubUpdate()).withBroadcastSet(loop, "parameters")
+				// sum up all the steps
+				.reduce(new UpdateAccumulator(numberOfFeatures))
+				// average the steps and update all parameters
+				.map(new Update());
+
+		// feed new parameters back into next iteration
+		DataSet<Params> final_params = loop.closeWith(newParameters);
+
+		//read train data-set from csv
+		DataSet<Tuple1<String>> test_csv = env.readCsvFile(params.get("test")).fieldDelimiter("\t").ignoreFirstLine().types(String.class);
+		//convert to Data (X, y)
+		DataSet<Data> test_data = test_csv.flatMap(new ConvertToIndexMatrix());
+		//evaluate results
+		DataSet<String> results = test_data
+				.map(new Predict()).withBroadcastSet(final_params, "params")
+				.reduce(new Evaluate())
+				.map(new ComputeMetrics());
+
+		// emit result
+		JobExecutionResult perf_res = null;
+		if (params.has("output")) {
+
+			results
+					.writeAsText(params.get("output"), FileSystem.WriteMode.OVERWRITE);
+
+			// since file sinks are lazy, we trigger the execution explicitly
+			perf_res = env.execute("Exus Use Case");
+		} else {
+			System.out.println("Printing result to stdout. Use --output to specify output path.");
+			results.print();
+		}
+
+		System.out.println(String.format("LR Job runtime: %d ms", perf_res
+				.getNetRuntime(TimeUnit.MILLISECONDS)));
+
+	}
+
+	// *************************************************************************
+	//     DATA SOURCE READING (POINTS AND CENTROIDS)
+	// *************************************************************************
+	static class Data implements Serializable {
+		double[] X;
+		double y;
+
+		Data(double[] x, double y) {
+			X = x;
+			this.y = y;
+		}
+	}
+
+	/**
+	 * A set of parameters -- theta_0, theta_1, ... , theta_n
+	 */
+
+	static class Params {
+		double[] W;
+		final int n = 82;
+
+		Params(double[] w) {
+			W = w;
+		}
+
+		public Params div(Integer a) {
+			for (int j = 0; j < n + 1; j++) {
+				this.W[j] = W[j] / a;
+			}
+			return this;
+		}
+	}
+
+
+	public static class ConvertToIndexMatrix implements FlatMapFunction<Tuple1<String>, Data> {
+
+		final int n = 82;
+
+
+		@Override
+		public void flatMap(Tuple1<String> stringTuple1, Collector<Data> collector) {
+			String line = stringTuple1.f0;
+			int col = -1;
+			double[] X = new double[n + 1];
+			double y = -1.0;
+			for (String cell : line.split(",")) {
+
+				//when col is -1 , then take the row number , else collect values
+				if (col > -1 && col < n) {
+					X[col] = cell != null ? Double.parseDouble(cell) : 0.0;
+				} else if (col == n) {
+					X[col] = 1.0;
+					y = Double.parseDouble(cell);
+				}
+				col++;
+			}
+
+			collector.collect(new Data(X, y));
+		}
+	}
+
+	// *************************************************************************
+	//     DATA TYPES
+	// *************************************************************************
+
+	// *************************************************************************
+	//     USER FUNCTIONS
+	// *************************************************************************
+
+	/**
+	 * Compute a single BGD type update for every parameters.
+	 */
+	public static class SubUpdate extends RichMapFunction<Data, Tuple2<Params, Integer>> {
+
+		Collection<Params> parameters;
+
+		int count = 1;
+
+		final int n = 82;
+		final double lr = 0.1;
+
+
+		/**
+		 * Reads the parameters from a broadcast variable into a collection.
+		 */
+		@Override
+		public void open(Configuration parameters) {
+			this.parameters = getRuntimeContext().getBroadcastVariable("parameters");
+		}
+
+		@Override
+		public Tuple2<Params, Integer> map(Data in) {
+
+			Params parameter = this.parameters.iterator().next();
+
+			double z = 0.0;
+			for (int j = 0; j < n + 1; j++) {
+				z += in.X[j] * parameter.W[j];
+			}
+
+			double error = (double) 1 / (1 + Math.exp(-z)) - in.y;
+
+			for (int j = 0; j < n + 1; j++) {
+				in.X[j] = parameter.W[j] - lr * (error * in.X[j]);
+			}
+
+			return new Tuple2<>(new Params(in.X), count);
+
+		}
+	}
+
+	/**
+	 * Compute TP,TN,FP,FN
+	 */
+	public static class Predict extends RichMapFunction<Data, Tuple4<Integer, Integer, Integer, Integer>> {
+
+		Collection<Params> parameters;
+
+		Params parameter;
+
+		final int n = 82;
+
+
+		/**
+		 * Reads the parameters from a broadcast variable into a collection.
+		 */
+		@Override
+		public void open(Configuration parameters) {
+			this.parameters = getRuntimeContext().getBroadcastVariable("params");
+		}
+
+		@Override
+		public Tuple4<Integer, Integer, Integer, Integer> map(Data in) {
+
+			this.parameter = this.parameters.iterator().next();
+
+			double z = 0.0;
+			for (int j = 0; j < n + 1; j++) {
+				z += in.X[j] * parameter.W[j];
+			}
+
+			double predict = ((double) 1 / (1 + Math.exp(-z))) > 0.5 ? 1.0 : 0.0;
+
+			if (predict == 0.0 && in.y == 0.0) {
+				return new Tuple4<>(0, 1, 0, 0); // tn
+			} else if (predict == 0.0 && in.y == 1.0) {
+				return new Tuple4<>(0, 0, 0, 1); // fn
+			} else if (predict == 1.0 && in.y == 0.0) {
+				return new Tuple4<>(0, 0, 1, 0); // fp
+			} else if (predict == 1.0 && in.y == 1.0) {
+				return new Tuple4<>(1, 0, 0, 0); // tp
+			} else {
+				return new Tuple4<>(0, 0, 0, 0); // tp
+			}
+
+		}
+	}
+
+	/**
+	 * Accumulator all the update.
+	 */
+	public static class UpdateAccumulator implements ReduceFunction<Tuple2<Params, Integer>> {
+
+		private int n;
+
+		UpdateAccumulator(int n) {
+			this.n = n;
+		}
+
+		@Override
+		public Tuple2<Params, Integer> reduce(Tuple2<Params, Integer> val1, Tuple2<Params, Integer> val2) {
+
+			for (int j = 0; j < n + 1; j++) {
+				val1.f0.W[j] = val1.f0.W[j] + val2.f0.W[j];
+			}
+			return new Tuple2<>(val1.f0, val1.f1 + val2.f1);
+
+		}
+	}
+
+	public static class Evaluate implements ReduceFunction<Tuple4<Integer, Integer, Integer, Integer>> {
+
+		@Override
+		public Tuple4<Integer, Integer, Integer, Integer> reduce(Tuple4<Integer, Integer, Integer, Integer> val1, Tuple4<Integer, Integer, Integer, Integer> val2) {
+			return new Tuple4<>(val1.f0 + val2.f0, val1.f1 + val2.f1, val1.f2 + val2.f2, val1.f3 + val2.f3);
+		}
+	}
+
+	/**
+	 * Compute the final update by average them.
+	 */
+	public static class Update implements MapFunction<Tuple2<Params, Integer>, Params> {
+
+		@Override
+		public Params map(Tuple2<Params, Integer> arg0) {
+			return arg0.f0.div(arg0.f1);
+		}
+
+	}
+
+
+	public static class ComputeMetrics implements MapFunction<Tuple4<Integer, Integer, Integer, Integer>, String> {
+		@Override
+		public String map(Tuple4<Integer, Integer, Integer, Integer> v) {
+			double acc = 1.0 - (double) (v.f2 + v.f3) / (v.f0 + v.f1 + v.f2 + v.f3);
+			double pr = (double) v.f0 / (v.f0 + v.f2);
+			double rec = (double) v.f0 / (v.f0 + v.f3);
+			double f1 = (2 * pr * rec) / (pr + rec);
+			return "ACCURACY: " + acc + "\n" + "PRECISION: " + pr + "\n" + "RECALL: " + rec + "\n" + "f1 MEASURE: " + f1 + "\n";
+		}
+
+	}
+}
